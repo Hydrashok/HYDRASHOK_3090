c60eed0cc8641a1052e8cf8e8936b90038b3c709
Hydrashok <32787289+Hydrashok@users.noreply.github.com>
2017-10-20 22:21:14 -0400

    Redoing audio Manager

--- HYDRASHOK_3090/Assets/_Scripts/LevelManager.cs
+++ HYDRASHOK_3090/Assets/_Scripts/LevelManager.cs
@@ -5,20 +5,23 @@ using UnityEngine.Audio;
 
 public class LevelManager : MonoBehaviour
 {
-    
+   public MusicManager musicmanager;
 
     int i;
     
     public void Start()
     {
        i = SceneManager.GetActiveScene().buildIndex;  // get scene by index
+        FindObjectOfType<MusicManager>().Play("Title");
     }
     
     public void LoadLevel(string name)
     {
-		Debug.Log ("New Level load: " + name);
+		
         SceneManager.LoadSceneAsync(name);
-	}
+        Debug.Log("New Level load: " + name);
+        musicmanager.MusicCheck();
+    }
 
     public void LoadNextLevel()
     {
